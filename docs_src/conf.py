3f385d99f1ba4522b0f2c8e1bb60eb471f90c201
Plutonium-239 <plutonium.239.811@gmail.com>
2024-04-14 01:30:22 +0530

    fix readthedocs errors

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -2,6 +2,8 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os, sys
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
